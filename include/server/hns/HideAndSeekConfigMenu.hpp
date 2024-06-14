5fe66fd3df4275bea649ddc57fc2e7330c1d3d72
Robin C. Ladiges <rcl.git@blackpinguin.de>
2024-06-14 11:21:27 +0200

    refac: whitespace

--- include/server/hns/HideAndSeekConfigMenu.hpp
+++ include/server/hns/HideAndSeekConfigMenu.hpp
@@ -7,8 +7,8 @@
 class HideAndSeekConfigMenu : public GameModeConfigMenu {
 public:
     HideAndSeekConfigMenu();
-    
-    void initMenu(const al::LayoutInitInfo &initInfo) override;
+
+    void initMenu(const al::LayoutInitInfo& initInfo) override;
     const sead::WFixedSafeString<0x200>* getStringData() override;
     GameModeConfigMenu::UpdateAction updateMenu(int selectIndex) override;
 
